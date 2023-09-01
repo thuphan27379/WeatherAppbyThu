e553c7074a03fdddf1fa16ebdfdfada9ee8774eb
thuphan27379 <131003628+thuphan27379@users.noreply.github.com>
2023-09-01 07:47:29 +0700

    tra lai cho dung file counter app

--- src/App.js
+++ src/App.js
@@ -1,86 +1,52 @@
-// fetch data from API with useEffect
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react"; // rfce
+// remember:
+// -export at the end
+import "./App.css";
 
-const api = {
-  key: "1acde3f7d0c9db2f0b808ca933d86888",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
+export default function App() {
+  // declare state at the begining of component
+  const [count, setCount] = useState(0); //value
 
-// function fetch data: fetchWeatherData
-function App() {
-  const [searchInput, setSearchInput] = useState(""); //input value
-  const [searchCity, setSearchCity] = useState("");
-  const [weatherInfo, setWeatherInfo] = useState(""); //ket qua cua fetch
-  const [loading, setLoading] = useState("false"); //dang cho doi
-  const [errorMessage, setErrorMessage] = useState(""); //thong bao khi co loi
+  let color = count > 0 ? "green" : count < 0 ? "red" : "black";
+  /*if (count > 0) {
+    //change color
+    color = "green";
+  }
+  if (count < 0) {
+    color = "red";
+  }
+  if (count === 0) {
+    color = "black";
+  }*/
 
-  useEffect(() => {
-    // fetch data
-    const fetchWeatherData = async () => {
-      // use async function
-      if (!searchCity) return;
-      // console.log("Searching")
-      //
-      setLoading(true); //bat dau
-      // loading process, nam giua 2 setloading bat dau va ket thuc
-      try {
-        const url = `${api.base}weather?q=${searchCity}&units=metric&appid=${api.key}`;
-        const response = await fetch(url);
-        const data = await response.json();
-        if (response.ok) {
-          // setWeatherInfo(JSON.stringify(data));
-          setWeatherInfo(
-            `${data.name} ${data.sys.country}, ${data.weather[0].description}, ${data.main.temp} degree.`
-          );
-          setErrorMessage("");
-        } else {
-          setErrorMessage(data.message);
-        }
-      } catch (error) {
-        setErrorMessage(error.message);
-      }
-      setLoading(false); //ket thuc
-      //
-    };
-    fetchWeatherData(); //goi function ngay ben trong useEffect nay
-  }, [searchCity]); //dependency chi khi searchcity thay doi, chi thuc hien sau khi click button
-
-  const handleSubmit = (e) => {
-    //step 2:
-    e.preventDefault();
-    setSearchCity(searchInput); //goi function setsearchcity voi gia tri la searchinput
-  };
+  // functions
+  const increment = () => setCount(count + 1); //add event
+  const descrement = () => setCount(count - 1);
+  const resetCount = () => setCount(0);
 
   return (
-    //render
-    <>
-      <h1>Fetch data from API with useEffect</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          // step 1: get value & click
-          // key={}
-          type="text"
-          placeholder="City"
-          value={searchInput} //value
-          onChange={(e) => setSearchInput(e.target.value)} //event
-        />
-        &nbsp;
-        <button>Search</button>
-      </form>
-      <br />
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          {errorMessage ? (
-            <div style={{ color: "red" }}>{errorMessage}</div>
-          ) : (
-            <div>{weatherInfo}</div>
-          )}
-        </>
-      )}
-    </>
+    // render
+    // dat state truc tiep vao trong element html
+    // dat even handle truc tiep vao trong element html
+    <div className="container">
+      <h1>Counter</h1>
+      <span style={{ color: color }} id="value">
+        {count}
+      </span>
+      <div class="btn-group">
+        <button onClick={descrement} class="btn btn-decrease">
+          DECREASE
+        </button>
+        <button onClick={resetCount} class="btn btn-reset">
+          RESET
+        </button>
+        <button onClick={increment} class="btn btn-increase">
+          INCREASE
+        </button>
+      </div>
+    </div>
   );
 }
 
-export default App;
+// export default App;
+//
