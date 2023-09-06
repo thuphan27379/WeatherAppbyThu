e7d75ab3362ee4354c44fb87c6556dffebcce459
thuphan27379 <131003628+thuphan27379@users.noreply.github.com>
2023-09-06 12:57:14 +0700

    upload git 1st

--- src/App.js
+++ src/App.js
@@ -1,52 +1,75 @@
-import React, { useState } from "react"; // rfce
-// remember:
-// -export at the end
-import "./App.css";
+import React, { useState, useEffect } from "react";
 
-export default function App() {
-  // declare state at the begining of component
-  const [count, setCount] = useState(0); //value
+const api = {
+  key: "1acde3f7d0c9db2f0b808ca933d86888",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
-  let color = count > 0 ? "green" : count < 0 ? "red" : "black";
-  /*if (count > 0) {
-    //change color
-    color = "green";
-  }
-  if (count < 0) {
-    color = "red";
-  }
-  if (count === 0) {
-    color = "black";
-  }*/
+function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [seachCity, setSearchCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("");
 
-  // functions
-  const increment = () => setCount(count + 1); //add event
-  const descrement = () => setCount(count - 1);
-  const resetCount = () => setCount(0);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchCity(searchInput);
+  };
+  //
+  useEffect(() => {
+    //fetch data
+    const fetchWeatherData = async () => {
+      if (!seachCity) return;
+      // process
+      setLoading(true);
+      try {
+        const url = `${api.base}weather?q=${seachCity}&units=metric&appid=${api.key}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (response.ok) {
+          setWeatherInfo(
+            `City: ${data.name}, ${data.sys.country}. Weather: ${data.weather[0].description}. Temparature: ${data.main.temp} degree.`
+          );
+          setErrorMessage("");
+        } else {
+          setErrorMessage(data.message);
+        }
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+      setLoading(false);
+      //
+    };
+    fetchWeatherData();
+  }, [seachCity]);
+  //
 
   return (
-    // render
-    // dat state truc tiep vao trong element html
-    // dat even handle truc tiep vao trong element html
-    <div className="container">
-      <h1>Counter</h1>
-      <span style={{ color: color }} id="value">
-        {count}
-      </span>
-      <div class="btn-group">
-        <button onClick={descrement} class="btn btn-decrease">
-          DECREASE
-        </button>
-        <button onClick={resetCount} class="btn btn-reset">
-          RESET
-        </button>
-        <button onClick={increment} class="btn btn-increase">
-          INCREASE
-        </button>
-      </div>
-    </div>
+    <>
+      <h1>Fetch Data with useEffect</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="city"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <>
+          {errorMessage ? (
+            <div style={{ color: "red" }}>{errorMessage}</div>
+          ) : (
+            <div>{weatherInfo}</div>
+          )}
+        </>
+      )}
+    </>
   );
 }
 
-// export default App;
-//
+export default App;
